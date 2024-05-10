4bd11f6ba16105c5bb5a94995b19ae670d5dc440
aplank <aplank@Inception.aplank.42.fr>
2024-05-10 20:46:19 +0200

    can connect to mariadb, nightly commit

--- srcs/MariaDB/run_maria.bash
+++ srcs/MariaDB/run_maria.bash
@@ -2,10 +2,15 @@
 #stops script when any command fails
 set -e
 
+#mariadb-install-db
 /etc/init.d/mariadb start
-#mariadb
+mariadb < create_users.sql
+/etc/init.d/mariadb stop
+mysqld --user=mysql
 
+
+echo servus
 # Keep the container running
 #exec "$@"
 
-sleep infinity
+#sleep infinity
