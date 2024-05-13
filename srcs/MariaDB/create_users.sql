a4b4a0fbb7fb405fb1891316234707ca3b2258cb
AnnaFiona <annafiona.plank@gmail.com>
2024-05-13 20:25:56 +0200

    don't know if WordPress image works, can't run with docker-compose

--- srcs/MariaDB/create_users.sql
+++ srcs/MariaDB/create_users.sql
@@ -1,5 +1,6 @@
-CREATE USER 'lisa'@'172.30.0.%' IDENTIFIED BY 'root_password';
-GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'172.30.0.%' WITH GRANT OPTION;
+CREATE DATABASE wordpress_db --what if already in database?
+CREATE USER 'lisa'@'%' IDENTIFIED BY 'root_password';
+GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 -- needs to be variable???
