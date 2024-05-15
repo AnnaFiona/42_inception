5b0e6a8c946d7b6703872b59895be9c881942ea1
AnnaFiona <annafiona.plank@gmail.com>
2024-05-15 17:02:15 +0200

    nightly commit

--- srcs/WordPress/setup_wp.bash
+++ srcs/WordPress/setup_wp.bash
@@ -1,11 +1,22 @@
 #!/bin/bash
-wget http://wordpress.org/latest.tar.gz
-tar xfz latest.tar.gz
-mv wordpress/* ./
-rm -f latest.tar.gz
-rm index.html
-# rm wordpress ?
+if ! [ -d "/var/www/html" ]; then
+    mkdir /var/www/html
+    echo "made /var/www/html"
+fi
 
-#rm setup_wp.bash  #?
+if ! [ -d "/var/www/html/wordpress" ]; then
+    wget -q -P /var/www/html/ http://wordpress.org/latest.tar.gz
+    tar xfz /var/www/html/latest.tar.gz -C /var/www/html/
+    #mv /var/www/html/wordpress/* /var/www/html/
+    rm -f /var/www/html/latest.tar.gz
+    chown -R www-data:www-data /var/www/html/wordpress/
+    #rm /var/www/html/index.html
+    # rm wordpress ?
+    echo "downloaded wordpress"
+fi
 
-wc
+if ! [ -f "/var/www/html/wordpress/wp-config.php" ]; then
+    cp wp-config.php /var/www/html/wordpress
+fi
+
+sleep infinity
