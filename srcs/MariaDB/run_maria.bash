8e9b2c9483b24cd257f26d5077eae671ab272411
AnnaFiona <annafiona.plank@gmail.com>
2024-05-24 19:19:37 +0200

    nightly commit

--- srcs/MariaDB/run_maria.bash
+++ srcs/MariaDB/run_maria.bash
@@ -4,18 +4,17 @@
 
 #mariadb-install-db
 /etc/init.d/mariadb start
-#mariadb < create_users.sql
 
 #mysql -u mysql -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE wordpress_db;";
 #mysql -u mysql -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER 'lisa'@'%' IDENTIFIED BY 'root_password'";
 #mysql -u mysql -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'%' WITH GRANT OPTION";
 #mysql -u mysql -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES";
 echo "1"
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS wordpress_db;";
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};";
 echo "2"
-mariadb -u root -e "CREATE USER IF NOT EXISTS 'lisa'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'";
+mariadb -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'";
 echo "3"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'lisa'@'%' WITH GRANT OPTION";
+mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION";
 echo "4"
 mariadb -u root -e "FLUSH PRIVILEGES";
 echo "5"
