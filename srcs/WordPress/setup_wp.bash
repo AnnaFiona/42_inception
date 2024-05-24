40f63beb47cf9acde724f815e18c0a05cd9a0d6f
AnnaFiona <annafiona.plank@gmail.com>
2024-05-24 17:49:11 +0200

    can connect to wordpress(php-fpm) via internet over nginx

--- srcs/WordPress/setup_wp.bash
+++ srcs/WordPress/setup_wp.bash
@@ -7,11 +7,8 @@ fi
 if ! [ -d "/var/www/html/wordpress" ]; then
     wget -q -P /var/www/html/ http://wordpress.org/latest.tar.gz
     tar xfz /var/www/html/latest.tar.gz -C /var/www/html/
-    #mv /var/www/html/wordpress/* /var/www/html/
     rm -f /var/www/html/latest.tar.gz
     chown -R www-data:www-data /var/www/html/wordpress/
-    #rm /var/www/html/index.html
-    # rm wordpress ?
     echo "downloaded wordpress"
 fi
 
@@ -19,5 +16,4 @@ if ! [ -f "/var/www/html/wordpress/wp-config.php" ]; then
     cp wp-config.php /var/www/html/wordpress
 fi
 
-/etc/init.d/php8.2-fpm start
-sleep infinity
+/usr/sbin/php-fpm8.2 -F
