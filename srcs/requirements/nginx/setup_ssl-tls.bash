be4b8f632c6dd290e18f2bc85262daa5414a3b56
Anna Fiona Plank <annafiona.plank@gmail.com>
2024-06-16 15:24:29 +0200

    put a * in front of echos so i can tell which output is from me

--- srcs/requirements/nginx/setup_ssl-tls.bash
+++ srcs/requirements/nginx/setup_ssl-tls.bash
@@ -3,16 +3,16 @@
 #making ssl-certificates directory if it doesn't exist yet
 if ! [ -d /var/www/html/ssl-certificates/ ]; then
 	mkdir /var/www/html/ssl-certificates/;
-	echo "made ssl-certificates directory"
+	echo "* made ssl-certificates directory"
 fi
 
 #making self-signed ssl/tls-certificate if it doesn't exist
 if ! [ -f "/var/www/html/ssl-certificates/aplank.42.fr.crt" ]; then
 	openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout /var/www/html/ssl-certificates/aplank.42.fr.key -out /var/www/html/ssl-certificates/aplank.42.fr.pem -subj "/CN=aplank.42.fr";
 	openssl x509 -outform pem -in /var/www/html/ssl-certificates/aplank.42.fr.pem -out /var/www/html/ssl-certificates/aplank.42.fr.crt;
-	echo "made ssl/tls-certificate"
+	echo "* made ssl/tls-certificate"
 fi
 
 #starting nginx
-echo "starting nginx"
+echo "* starting nginx"
 nginx -g 'daemon off;'
--- srcs/requirements/wordpress/setup_wp.bash
+++ srcs/requirements/wordpress/setup_wp.bash
@@ -8,7 +8,7 @@ export MYSQL_USER_PASSWORD=$(cat /run/secrets/user_pw)
 #making directory for volume if it doesn't exist
 if ! [ -d "/var/www/html" ]; then
     mkdir /var/www/html
-    echo "made /var/www/html"
+    echo "* made /var/www/html"
 fi
 
 #installing wordpress if it isn't installed
@@ -17,13 +17,13 @@ if ! [ -d "/var/www/html/wordpress" ]; then
     tar xfz /var/www/html/latest.tar.gz -C /var/www/html/
     rm -f /var/www/html/latest.tar.gz
     chown -R www-data:www-data /var/www/html/wordpress/
-    echo "downloaded wordpress"
+    echo "* downloaded wordpress"
 fi
 
 #copying modified wp-config.php file if it isn't already there
 if ! [ -f "/var/www/html/wordpress/wp-config.php" ]; then
     cp wp-config.php /var/www/html/wordpress
-    echo "copied wp-config.php"
+    echo "* copied wp-config.php"
 fi
 
 #installing wp-cli if it isn't installed already
@@ -31,7 +31,7 @@ if ! [ -f "/usr/local/bin/wp/wp-cli.phar" ]; then
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
-    echo "installed wp-cli"
+    echo "* installed wp-cli"
 fi
 
 #doing wp core install and adding admin wp user if not done already
@@ -42,14 +42,14 @@ if ! wp user get "${MYSQL_ADMIN_USER}" --quiet --allow-root --path=/var/www/html
         --admin_user=${MYSQL_ADMIN_USER} \
         --admin_password=${MYSQL_ADMIN_PASSWORD} \
         --admin_email=${MYSQL_ADMIN_USER}@example.com
-    echo "did wp core install (made wp admin)"
+    echo "* did wp core install (made wp admin)"
 fi
 
 #adding normal wp user if not already there
 if ! wp user get "${MYSQL_USER}" --quiet --allow-root --path=/var/www/html/wordpress; then
     wp user create "${MYSQL_USER}" ${MYSQL_USER}@example.com --user_pass=${MYSQL_USER_PASSWORD} --allow-root --path=/var/www/html/wordpress
-    echo "made wp user"
+    echo "* made wp user"
 fi
 
-echo "starting wp"
+echo "* starting wp"
 /usr/sbin/php-fpm8.2 -F
