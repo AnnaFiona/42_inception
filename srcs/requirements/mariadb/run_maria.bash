a1402901ad31742909be1d1bdae0104fa5f6d77c
AnnaFiona <annafiona.plank@gmail.com>
2024-06-27 18:58:33 +0200

    mariadb docs say you don't need a root password if you use a version after 10.4

--- srcs/requirements/mariadb/run_maria.bash
+++ srcs/requirements/mariadb/run_maria.bash
@@ -6,8 +6,6 @@
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};";
 mariadb -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}'";
 mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ADMIN_USER}'@'%' WITH GRANT OPTION";
-#mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'";
-flush privileges;
 mariadb -u root -e "FLUSH PRIVILEGES";
 
 /etc/init.d/mariadb stop
