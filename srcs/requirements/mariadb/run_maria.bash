ef191cfa0c3c98637b6dc5a44028f293aaf2dae5
AnnaFiona <annafiona.plank@gmail.com>
2024-06-28 21:01:08 +0200

    mounted the mariadb volume to  wrong path in docker-compose file, "fixed" the mariadb bash so it doesn't always go in the if block

--- srcs/requirements/mariadb/run_maria.bash
+++ srcs/requirements/mariadb/run_maria.bash
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+#making mysqld dir and giving ownership to mysql user
+mkdir /run/mysqld
+chown mysql:mysql /run/mysqld
+
 #setting up mariadb-users if not done before (script won't leave the block when it enters, but it only happens the first time the script is run)
-if ! [ -f "create_users.sql" ]; then
+if ! [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
     #creating sql script
+    echo "* creating sql script"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
     CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';
     GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ADMIN_USER}'@'%' WITH GRANT OPTION;
     FLUSH PRIVILEGES;" > create_users.sql
 
-    #making mysqld dir and giving ownership to mysql user
-    mkdir /run/mysqld
-    chown mysql:mysql /run/mysqld
+    #giving ownership to mysql user
     chown mysql:mysql create_users.sql
 
     #starting the db with the sql script; this only needs to happen the first time it starts
